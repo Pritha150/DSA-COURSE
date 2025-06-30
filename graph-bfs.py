175efe410e7a4848e5139e565696061d17b759f8
Pritha150 <prithaadhikari150@gmail.com>
2025-06-30 12:47:39 +0530

    Create graph-bfs.py

---
+++ graph-bfs.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        current_vertex = queue.popleft()
+        print(current_vertex, end=' ')
+
+        for neighbor in graph[current_vertex]:
+            if neighbor not in visited:
